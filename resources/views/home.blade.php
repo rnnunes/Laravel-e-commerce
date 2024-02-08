e919ddefd235812455858497e8784dd3d6df7b5c
rnnunes <ryannunes70@gmail.com>
2024-02-07 23:42:37 -0400

    commit1

---
+++ resources/views/home.blade.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home - My Shop</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+</head>
+<body>
+    
+        <nav class="navbar navbar-light navbar-expand-md bg-light pl-5 pr-5 mb-5">
+    <!-- <nav class="navbar navbar-light navbar-expand-mb bg-light pl-5 pr-5 mb-5"> -->
+        <a href="#" class="navbar-brand">Myshop</a>
+        <div class="collapse navbar-collapse">
+            <div class="navbar-nav">
+                <a class="nav-link" href="#">Home</a>
+                <a class="nav-link" href="#">Categorias</a>
+                <a class="nav-link" href="#">Cadastrar</a>
+            </div>
+        </div>
+        <a href="#" class="btn btn-sm"><i class="fa fa-shopping-cart"></i></a>
+    </nav>
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-3 mb-3">
+                <div class="card">
+                    <img src="https://media.pichau.com.br/media/catalog/product/cache/2f958555330323e505eba7ce930bdf27/p/g/pg-aps-wht013.jpg" class="card-img-top"/>
+                    <div class="card-body">
+                        <h6 class="card-title">Produto 01</h6>
+                        <a href="#" class="btn btn-sm btn-secondary">Adicionar Item</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-3 mb-3">
+                <div class="card">
+                    <img src="https://media.pichau.com.br/media/catalog/product/cache/2f958555330323e505eba7ce930bdf27/p/g/pg-aq120pro-argb013.jpg" class="card-img-top"/>
+                    <div class="card-body">
+                        <h6 class="card-title">Produto 02</h6>
+                        <a href="#" class="btn btn-sm btn-secondary">Adicionar Item</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-3 mb-3">
+                <div class="card">
+                    <img src="https://media.pichau.com.br/media/catalog/product/cache/2f958555330323e505eba7ce930bdf27/1/3/13747-29.jpg" class="card-img-top"/>
+                    <div class="card-body">
+                        <h6 class="card-title">Produto 03</h6>
+                        <a href="#" class="btn btn-sm btn-secondary">Adicionar Item</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-3 mb-3">
+                <div class="card">
+                    <img src="https://media.pichau.com.br/media/catalog/product/cache/2f958555330323e505eba7ce930bdf27/s/t/streak-v1-pt4.jpg" class="card-img-top"/>
+                    <div class="card-body">
+                        <h6 class="card-title">Produto 04</h6>
+                        <a href="#" class="btn btn-sm btn-secondary">Adicionar Item</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-3 mb-3">
+                <div class="card">
+                    <img src="https://media.pichau.com.br/media/catalog/product/cache/2f958555330323e505eba7ce930bdf27/p/c/pch-ps100-24prgbwht3.jpg" class="card-img-top"/>
+                    <div class="card-body">
+                        <h6 class="card-title">Produto 05</h6>
+                        <a href="#" class="btn btn-sm btn-secondary">Adicionar Item</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-3 mb-3">
+                <div class="card">
+                    <img src="https://media.pichau.com.br/media/catalog/product/cache/2f958555330323e505eba7ce930bdf27/c/g/cgr-psdelrgb-w-3604.jpg" class="card-img-top"/>
+                    <div class="card-body">
+                        <h6 class="card-title">Produto 06</h6>
+                        <a href="#" class="btn btn-sm btn-secondary">Adicionar Item</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-3 mb-3">
+                <div class="card">
+                    <img src="https://media.pichau.com.br/media/catalog/product/cache/2f958555330323e505eba7ce930bdf27/z/n/zns-zctlav2-bl014.jpg" class="card-img-top"/>
+                    <div class="card-body">
+                        <h6 class="card-title">Produto 071</h6>
+                        <a href="#" class="btn btn-sm btn-secondary">Adicionar Item</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-3 mb-3">
+                <div class="card">
+                    <img src="https://media.pichau.com.br/media/catalog/product/cache/2f958555330323e505eba7ce930bdf27/1/3/13950-88.jpg" class="card-img-top"/>
+                    <div class="card-body">
+                        <h6 class="card-title">Produto 08</h6>
+                        <a href="#" class="btn btn-sm btn-secondary">Adicionar Item</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+
+</body>
+</html>
